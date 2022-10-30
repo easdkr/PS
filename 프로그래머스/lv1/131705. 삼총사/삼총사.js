d70d445f97311e1342fb70169103ff38d4e25afc
June <easdkr@daum.net>
2022-10-30 17:39:24 +0900

    [level 1] Title: 삼총사, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,24 +1,21 @@
 /**
- *
  * @param {number[]} number
  */
 function solution(number) {
-  var answer = 0;
-
   /**
-   *
    * @param {number[]} picked
    * @param {number} start
    * @returns {number}
    */
   const combination = (picked, start) => {
-    if (picked.length === 3) {
+    if (picked.length === 3)
       return picked.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
-    }
+
     let result = 0;
-    for (let i = start; i < number.length; i++) {
+
+    for (let i = start, length = number.length; i < length; i++)
       result += combination([...picked, number[i]], i + 1);
-    }
+
     return result;
   };
 
