0f4add08c0ffc1989590223da42cbf1d699c9cf3
June <easdkr@daum.net>
2023-03-21 23:29:45 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.29 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,25 @@
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
+function solution(maps) {
+    let answer = 1;
+    const visited = maps;
+    const queue = [];
+    const n = maps.length, m = maps[0].length;
+    queue.push([0, 0]);
+    while (queue.length > 0) {
+        for (let j = 0, len = queue.length; j < len; ++j) {
+            const [x, y] = queue.shift();
+            for (let i = 0; i < 4; ++i) {
+                const nx = x + dx[i], ny = y + dy[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && visited[nx][ny] === 1) {
+                    if (nx === n - 1 && ny === m - 1)
+                        return ++answer;
+                    queue.push([nx, ny]);
+                    visited[nx][ny] = 0;
+                }
+            }
+        }
+        ++answer;
+    }
+    return -1;
+}
