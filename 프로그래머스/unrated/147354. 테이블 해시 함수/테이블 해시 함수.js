ebebfd2a9b013b96808af3d877ba0754860043af
June <easdkr@daum.net>
2023-03-22 18:29:57 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -0,0 +1,7 @@
+
+function solution(data, col, row_begin, row_end) {
+    return data.sort((a, b) => a[col-1] !== b[col-1] ? a[col-1] - b[col-1] : b[0] - a[0])
+    .filter((_, index) => index >= row_begin -1 && index <= row_end -1)
+    .map((row, index) => row.reduce((prev, curr) => prev += curr % (index + row_begin), 0))
+    .reduce((prev, curr) => prev ^ curr, 0);
+}
